783f0b9776d2c7087d485ce7da0b18a083cdc37d
dzakysyaam <mdzakysyam1204@gmail.com>
2025-08-05 03:22:17 +0700

    init

--- utils/utils.go
+++ utils/utils.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"	
 	"net/http"	
 )
-
+//respondjson
 func RespondJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
